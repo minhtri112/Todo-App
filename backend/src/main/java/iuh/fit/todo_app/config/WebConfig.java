0b5ad262cd4cc65c287f34a46437674b0f377753
Tran Minh Tri <minhtri084038@gmail.com>
2026-07-06 22:53:39 +0700

    test deploy

--- backend/src/main/java/iuh/fit/todo_app/config/WebConfig.java
+++ backend/src/main/java/iuh/fit/todo_app/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://34.235.153.246")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
